d1eb4e9eb076c933b68983a37bd9dac2d466cea7
FreeLANMan <43796655+FreeLANMan@users.noreply.github.com>
2019-05-22 21:15:37 -0300

    Update and rename cronometro to cronometro.ino

--- Apps/cronometro.ino
+++ Apps/cronometro.ino
@@ -10,41 +10,46 @@
 void setup() { 
 
 // app cronometro
-  byte vez = 1;
   byte start = 0;
+  int horas = 0;
+  int minutos = 0;
+  int segundos = 0;
 }
 
 
 // -------------------------------------------
-// Código do programa que normalmente fica no 'void loop()' -
+// Código do programa que normalmente ficaria no 'void loop()' (mas no QuaSO fica antes)
 // -------------------------------------------
-void Aplicativo1() { 
+void Aplicativo1() { // troque por um número de aplicativo livre como 'Aplicativo2()'
   // cronometro para Quaso
   tela = 1;
-  if (vez == 1) {
+  if (start == 0) {
     display.drawString(rnd1,0,"Cronometro" );
     display.drawString(rnd1,20,"Clique OK para iniciar a contagem" );
     if ((digitalRead(botao3) == HIGH) && (tela == 1)) {
       long inicio = millis() / 1000; // quantos segundos passaram desde que iniciou o sistema
       byte start = 1; }
-      
-  if (start = 1) { // se já começou a contagem
+  }
+  else if (start = 1) { // se já começou a contagem
     long tsegs = (millis()/1000) - inicio; // segundos desde que foi dado o start
     if (tsegs > 3600) { // já se passaram horas
-      int horas = int(tsegs) / 3600;
-      int minutos = (int(tsegs) % 3600) / 60;
-      int segundos = (minutos % 60);
+      horas = int(tsegs) / 3600;
+      minutos = (int(tsegs) % 3600) / 60;
+      segundos = (minutos % 60);
       display.drawString(rnd1,30,"String(horas) + ":" + String(minutos) + ":" + String(segundos));
-
-    else if ((tsegs > 60) { // já se passaram minutos
-      display.drawString(rnd1,30,"Tempo ativo: " + String(tsegs) + " minutos"); }
-    else {
-      display.drawString(rnd1,30,"Tempo ativo: " + String(tsegs) + " segundos");}
     }
-  
+    else if ((tsegs > 60) { // já se passaram minutos
+      minutos = int(tsegs) / 60;
+      segundos = (minutos % 60);
+      display.drawString(rnd1,30,"String(horas) + ":" + String(minutos) + ":" + String(segundos));
+    }
+    else {                      // já se passaram segundos
+      display.drawString(rnd1,30,"String(horas) + ":" + String(minutos) + ":" + String(tsegs));
+    }
     display.drawString(rnd1,40,"Aperte OK para parar a contagem";
+    if ((digitalRead(botao3) == HIGH) {
+      start = 0; // parou a contagem
+    }
   }
-    
   delay(400);
-  vez = 2;
 }
